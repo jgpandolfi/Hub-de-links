5b814d89059d8c6e99d3394fca41c66dfef94e01
Jota <jgpandolfi@gmail.com>
2025-01-02 03:00:37 -0300

    Hotfix

--- backend/server.js
+++ backend/server.js
@@ -319,8 +319,8 @@ fastify.post("/registrar-visitante", async (request, reply) => {
     const dadosVisitanteArray = Object.values(value)
 
     // Console log
+    console.log("DEBUG", dadosVisitante)
     console.log("DEBUG", value)
-    console.log("DEBUG", error)
 
     // Enviar query ao banco de dados
     const resultado = await pool.query(query, dadosVisitanteArray)
